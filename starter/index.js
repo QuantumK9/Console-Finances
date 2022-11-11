88511ef96db3b4a67311439de7bfb92ff2070f6d
QuantumK <quantumk9@hotmail.com>
2022-11-11 18:47:51 +0000

    rename displayCalculations function

--- starter/index.js
+++ starter/index.js
@@ -98,7 +98,7 @@ const calcTotalProfit = () => {
 };
 var totalProfit = calcTotalProfit();
 
-function calculations() {
+function displayCalculations() {
   console.log(`Financial Analysis
 ----------------------------
 Total Months: ${totalMonths}
@@ -108,5 +108,4 @@ Greatest Increase in Profits: Feb-2012 ($1926159)
 Greatest Decrease in Profits: Sep-2013 ($-2196167)`);
 }
 
-calculations();
-// totalProfit();
+displayCalculations();
